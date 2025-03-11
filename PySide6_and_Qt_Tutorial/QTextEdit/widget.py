88bd58b138b9ec182a10dbea187040b99e67a09f
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-11 02:51:51 +0100

    updated main.py

--- PySide6_and_Qt_Tutorial/QTextEdit/widget.py
+++ PySide6_and_Qt_Tutorial/QTextEdit/widget.py
@@ -10,33 +10,38 @@ class Widget(QWidget):
         #self.text_edit.textChanged.connect(self.text_changed) 
 
 
-        #Buttons for the QTextEdit
+        # Create buttons for the QTextEdit.
+
+        current_text_button = QPushButton("Current Text")
+        current_text_button.clicked.connect(self.current_text_button_clicked)
+
         copy_button = QPushButton("Copy")
-        #copy_button = copy_button.clicked.connect(self.text_edit.copy)
+        copy_button.clicked.connect(self.text_edit.copy) # Connect directly to the QTextEdit slot. 
 
         cut_button = QPushButton("Cut")
-        #cut_button = cut_button.clicked.connect(self.text_edit.cut)
+        cut_button.clicked.connect(self.text_edit.cut)
 
         paste_button = QPushButton("Paste")
-        #paste_button = paste_button.clicked.connect(self.text_edit.paste) # got through a custom slot
+        paste_button.clicked.connect(self.text_edit.paste) # got through a custom slot
 
         undo_button = QPushButton("Undo")
-        #undo_button = undo_button.clicked.connect(self.text_edit.undo)
+        undo_button.clicked.connect(self.text_edit.undo)
 
         redo_button = QPushButton("Redo")
-        #redo_button = redo_button.clicked.connect(self.text_edit.redo)
+        redo_button.clicked.connect(self.text_edit.redo)
 
         set_plain_text_button = QPushButton("Set Plain Text")
-        #set_plain_text_button = set_plain_text_button.clicked.connect(self.set_plain_text)
+        #set_plain_text_button.clicked.connect(self.set_plain_text)
 
         set_html_button = QPushButton("Set HTML")
-        #set_html_button = set_html_button.clicked.connect(self.set_html)
+        #set_html_button.clicked.connect(self.set_html)
 
         clear_button = QPushButton("Clear")
         #clear_button = clear_button.clicked.connect(self.text_edit.clear)
 
         # Create horizontal layout for the buttons.
         h_layout = QHBoxLayout()
+        h_layout.addWidget(current_text_button)
         h_layout.addWidget(copy_button)
         h_layout.addWidget(cut_button)
         h_layout.addWidget(paste_button)
@@ -52,3 +57,7 @@ class Widget(QWidget):
         v_layout.addWidget(self.text_edit)
         
         self.setLayout(v_layout)
+    
+    def current_text_button_clicked(self):
+        print(self.text_edit.toPlainText()) # Print the current text in terminal when the button is clicked. 
+
