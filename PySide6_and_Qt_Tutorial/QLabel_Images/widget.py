fb00303d912c8bd1f4df65cb39370de784670d6a
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-18 20:28:22 +0100

    Update widget files

---
+++ PySide6_and_Qt_Tutorial/QLabel_Images/widget.py
@@ -0,0 +1,7 @@
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        
