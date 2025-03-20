0ff863d5d1e104c9f7fe0beb0c81e153daa313c1
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-20 21:56:07 +0100

    new changes

--- PySide6_and_Qt_Tutorial/SizePolicies/widget.py
+++ PySide6_and_Qt_Tutorial/SizePolicies/widget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QSizePolicy
+from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QSizePolicy, QLineEdit, QPushButton
 
 class Widget(QWidget):
     def __init__(self):
@@ -11,10 +11,10 @@ class Widget(QWidget):
         label = QLabel("Some text : ")
         label_edit = QLineEdit()
 
-        h_layout = QHBoxLayout()
-        h_layout1.addWidget(label)
-        h_layout1.addWidget(label_edit)
-        
+        h_layout_1 = QHBoxLayout()
+        h_layout_1.addWidget(label)
+        h_layout_1.addWidget(label_edit)
+            
 
         button_1 = QPushButton("One")
         button_2 = QPushButton("Two")
@@ -23,8 +23,12 @@ class Widget(QWidget):
         #strecth : how much of the available(in the layout) is occupied by the widget
         #You specify the stretch when you add things to the layout : button_1 takes up 2/3 of the available space
         # button_2 and button_3 take up to 1/3 of the available space
-        
+
         h_layout_2 = QHBoxLayout()
         h_layout_2.addWidget(button_1,2)
         h_layout_2.addWidget(button_2,1)
         h_layout_2.addWidget(button_3,1)
+
+        v_layout = QVBoxLayout(self)
+        v_layout.addLayout(h_layout_1)
+        v_layout.addLayout(h_layout_2)
