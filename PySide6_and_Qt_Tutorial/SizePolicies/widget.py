53686c09fdd383c5c56c7edf4d4ecec50eb66d8f
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-20 21:38:37 +0100

    adding buttons

--- PySide6_and_Qt_Tutorial/SizePolicies/widget.py
+++ PySide6_and_Qt_Tutorial/SizePolicies/widget.py
@@ -15,3 +15,16 @@ class Widget(QWidget):
         h_layout1.addWidget(label)
         h_layout1.addWidget(label_edit)
         
+
+        button_1 = QPushButton("One")
+        button_2 = QPushButton("Two")
+        button_3 = QPushButton("Three")
+
+        #strecth : how much of the available(in the layout) is occupied by the widget
+        #You specify the stretch when you add things to the layout : button_1 takes up 2/3 of the available space
+        # button_2 and button_3 take up to 1/3 of the available space
+        
+        h_layout_2 = QHBoxLayout()
+        h_layout_2.addWidget(button_1,2)
+        h_layout_2.addWidget(button_2,1)
+        h_layout_2.addWidget(button_3,1)
