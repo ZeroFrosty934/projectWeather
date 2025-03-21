4f5e04f235173323ff97d2a6a0c5a334d640c0a1
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-21 09:17:24 +0100

    adding stetch factors

--- PySide6_and_Qt_Tutorial/SizePolicies/widget.py
+++ PySide6_and_Qt_Tutorial/SizePolicies/widget.py
@@ -8,12 +8,24 @@ class Widget(QWidget):
         self.setWindowTitle("Size Policies and stretches")
 
         # Size policy : how the widgets behaves if container space is expanded or shrunk
+        
         label = QLabel("Some text : ")
-        label_edit = QLineEdit()
+        line_edit = QLineEdit()
+
+        label_0 = QLabel("Some text_0 : ")
+        line_edit1 = QLineEdit()
+
+        # Size policy : how the widgets behaves if container space is expanded or shrunk
+        line_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
         h_layout_1 = QHBoxLayout()
         h_layout_1.addWidget(label)
-        h_layout_1.addWidget(label_edit)
+        h_layout_1.addWidget(line_edit)
+
+        h_layout_3 = QHBoxLayout()
+        h_layout_3.addWidget(label_0)
+        h_layout_3.addWidget(line_edit1)
             
 
         button_1 = QPushButton("One")
@@ -24,11 +36,14 @@ class Widget(QWidget):
         #You specify the stretch when you add things to the layout : button_1 takes up 2/3 of the available space
         # button_2 and button_3 take up to 1/3 of the available space
 
+        # The number after the widget is the stretch factor
         h_layout_2 = QHBoxLayout()
-        h_layout_2.addWidget(button_1,2)
+        h_layout_2.addWidget(button_1,2) 
         h_layout_2.addWidget(button_2,1)
         h_layout_2.addWidget(button_3,1)
 
         v_layout = QVBoxLayout(self)
         v_layout.addLayout(h_layout_1)
+        v_layout.addLayout(h_layout_3)
         v_layout.addLayout(h_layout_2)
+
