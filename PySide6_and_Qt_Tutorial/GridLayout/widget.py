3f12fc1f889fe4141852125800fbf2e76ef98fe1
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-22 17:55:36 +0100

    updated widget

--- PySide6_and_Qt_Tutorial/GridLayout/widget.py
+++ PySide6_and_Qt_Tutorial/GridLayout/widget.py
@@ -6,3 +6,12 @@ class Widget(QWidget):
         super().__init__()
 
         self.setWindowTitle("GridLayout Demo")
+
+        button_1 = QPushButton("One")
+        button_2 = QPushButton("Two")
+        button_3 = QPushButton("Three")
+        #button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        button_4 = QPushButton("Four")
+        button_5 = QPushButton("Five")
+        button_6 = QPushButton("Six")
+        button_7 = QPushButton("Seven")
