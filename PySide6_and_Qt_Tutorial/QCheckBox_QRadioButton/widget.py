1c6dc363b16ec2c4cd122ca43e0fe49c7b876c23
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-23 16:07:37 +0100

    Updated widget file

--- PySide6_and_Qt_Tutorial/QCheckBox_QRadioButton/widget.py
+++ PySide6_and_Qt_Tutorial/QCheckBox_QRadioButton/widget.py
@@ -6,3 +6,23 @@ class Widget(QWidget):
         super().__init__()
 
         self.setWindowTitle("QCheckBox and QRadioButton")
+
+        # Checkboxes : operation system
+        os = QGroupBox("Choose Operation System") 
+
+        # Create checkboxes for Windows, Linux and Mac
+        windows = QCheckBox("Windows")
+        windows.toggled.connect(self.windows_box_toggled) # Connect the toggled signal to a slot
+
+        linux = QCheckBox("Linux")
+        linux.toggled.connect(self.linux_box_toggled)
+
+        mac = QCheckBox("Mac")
+        mac.toggled.connect(self.mac_box_toggled)
+
+        os_layout = QVBoxLayout()
+        os_layout.addWidget(windows)
+        os_layout.addWidget(linux)
+        os_layout.addWidget(mac)
+        os.setLayout(os_layout)
+
