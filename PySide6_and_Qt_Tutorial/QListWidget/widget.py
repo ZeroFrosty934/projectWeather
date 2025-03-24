09261ddc5cfa4be9fbd647ce19e25598c9e70e94
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-24 13:40:29 +0100

    Update changes

---
+++ PySide6_and_Qt_Tutorial/QListWidget/widget.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QWidget, QCheckBox, QHBoxLayout, QVBoxLayout, QListWidget
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+
+        self.setWindowTitle("QListWidget Demo")
+
+    
