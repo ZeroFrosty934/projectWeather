bc8f02b8503920d8fefc014f0a028aabba37fc77
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-26 13:33:18 +0100

    Added def

--- PySide6_and_Qt_Tutorial/QListWidget/widget.py
+++ PySide6_and_Qt_Tutorial/QListWidget/widget.py
@@ -8,11 +8,20 @@ class Widget(QWidget):
 
         self.list_widget = QListWidget(self)
         self.list_widget.setSelectionMode(QAbstractItemView.MultiSelection)
-        self.list_widget.addItem("One")
-        self.list_widget.addItems(["Two", "Three"])
+        self.list_widget.addItem("One") # Add one item to the list
+        self.list_widget.addItems(["Two", "Three"]) # Add multiple items to the list at once.
+
+        self.list_widget.currentItemChanged.connect(self.current_item_changed) # Connect the current item changed signal to the current_item_changed definition.
+        self.list_widget.currentTextChanged.connect(self.current_text_changed) # Connect the current text changed signal to the current_text_changed definition.
 
 
         v_layout = QVBoxLayout(self)
         v_layout.addWidget(self.list_widget)
 
         self.setLayout(v_layout)
+
+    def current_item_changed(self, items): # This definition is called when the current item is changed.
+        print("Current item changed: ", items.text())
+
+    def current_text_changed(self, text): # This definition is called when the current text is changed. 
+        print("Current text changed: ", text)
