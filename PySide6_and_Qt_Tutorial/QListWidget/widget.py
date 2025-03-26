c86b74f695ff99623f26f6e1c5e10ba225ee0f4b
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-26 13:38:53 +0100

    added buttons

--- PySide6_and_Qt_Tutorial/QListWidget/widget.py
+++ PySide6_and_Qt_Tutorial/QListWidget/widget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QCheckBox, QHBoxLayout, QVBoxLayout, QListWidget, QAbstractItemView
+from PySide6.QtWidgets import QWidget, QCheckBox, QHBoxLayout, QVBoxLayout, QListWidget, QAbstractItemView, QPushButton
 
 class Widget(QWidget):
     def __init__(self):
@@ -14,9 +14,28 @@ class Widget(QWidget):
         self.list_widget.currentItemChanged.connect(self.current_item_changed) # Connect the current item changed signal to the current_item_changed definition.
         self.list_widget.currentTextChanged.connect(self.current_text_changed) # Connect the current text changed signal to the current_text_changed definition.
 
+        # Added buttons to the layout
+        button_add_item = QPushButton("Add Item")
+        button_add_item.clicked.connect(self.add_item)
 
+        button_delete_item = QPushButton("Delete Item")
+        button_delete_item.clicked.connect(self.delete_item)
+
+        button_item_count = QPushButton("Item Count")
+        button_item_count.clicked.connect(self.item_count)
+
+        button_selected_items = QPushButton("Selected Items")
+        button_selected_items.clicked.connect(self.selected_items)
+
+
+
+        # Layout for displaying the buttons on the screen
         v_layout = QVBoxLayout(self)
         v_layout.addWidget(self.list_widget)
+        v_layout.addWidget(button_add_item)
+        v_layout.addWidget(button_delete_item)
+        v_layout.addWidget(button_item_count)
+        v_layout.addWidget(button_selected_items)
 
         self.setLayout(v_layout)
 
