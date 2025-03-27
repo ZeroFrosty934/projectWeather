291ee6106b764f6aba1be24226eb191c94e388a1
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-27 11:47:35 +0100

    adding buttons

--- PySide6_and_Qt_Tutorial/QComboBox/widget.py
+++ PySide6_and_Qt_Tutorial/QComboBox/widget.py
@@ -4,5 +4,23 @@ class Widget(QWidget):
     def __init__(self):
         super().__init__()
 
+        # Set the title of the window
         self.setWindowTitle("QComboBox Demo")
-        
+
+        # Add planets to the ComboBox widget.
+        self.combo_box = QComboBox(self)
+        self.combo_box.addItem("Earth")
+        self.combo_box.addItem("Mars")
+        self.combo_box.addItem("Jupiter")
+        self.combo_box.addItem("Saturn")
+
+
+        # Buttons
+        button_current_value = QPushButton("Current Value")
+        button_current_value.clicked.connect(self.get_current_value)
+
+        button_set_value = QPushButton("Set Value")
+        button_set_value.clicked.connect(self.set_value)
+
+        button_get_value = QPushButton("Get Value")
+        button_get_value.clicked.connect(self.get_value)
