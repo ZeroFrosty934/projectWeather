12656986d85a247da8cfc6d5172d17a9dba41fd8
ZeroFrosty934 <victoronneling@gmail.com>
2025-03-27 10:14:53 +0100

    code added for tab widget

--- PySide6_and_Qt_Tutorial/QTabWidget/README.md
+++ PySide6_and_Qt_Tutorial/QTabWidget/README.md
@@ -1,3 +1,10 @@
 # QTabWidget
 
 * In This Demo, we will learn how to use QTabWidget. 
+
+
+
+* Package used:
+    - PySide6
+
+* Web Doc: https://doc.qt.io/qtforpython-6/PySide6/QtWidgets/QTableWidget.html#PySide6.QtWidgets.QTableWidget
--- PySide6_and_Qt_Tutorial/QTabWidget/widget.py
+++ PySide6_and_Qt_Tutorial/QTabWidget/widget.py
@@ -1,7 +1,38 @@
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QTabWidget, QPushButton
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QTabWidget, QPushButton, QLabel, QLineEdit
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
     
         self.setWindowTitle("QTabWidget Demo")
+
+        tab_widget = QTabWidget(self)
+
+
+        widget_form = QWidget() # Create a widget
+        label_full_name = QLabel("Full Name :")
+        line_edit_full_name = QLineEdit()
+
+        form_layout = QHBoxLayout() # Create a horizontal layout
+        form_layout.addWidget(label_full_name) 
+        form_layout.addWidget(line_edit_full_name)
+
+        widget_form.setLayout(form_layout) # Set layout to widget
+
+        # Buttons
+        widget_buttons = QWidget()
+        button_1 = QPushButton("One")
+        button_1.clicked.connect(self.on_button_clicked)
+
+        button_2 = QPushButton("Two")
+        button_3 = QPushButton("Three")
+
+        buttons_layout = QVBoxLayout()
+        buttons_layout.addWidget(button_1)
+        buttons_layout.addWidget(button_2)
+        buttons_layout.addWidget(button_3)
+
+        widget_buttons.setLayout(buttons_layout)
+
+    def on_button_clicked(self):
+        print("Button one clicked")
